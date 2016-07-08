be4373fa5a04fcaf1fb3c2c981a912dd6cf070e8
Chip Carnes <chip.carnes27@gmail.com>
2016-07-08 09:58:55 -0700

    Add new word form and associate route, success page and integration specs for word form

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ also_reload('lib/**/*.rb')
 require('./lib/word')
 
 get('/') do
+  @words = Word.all()
   erb(:index)
+end
+
+get('/words/new') do
+  erb(:words_form)
+end
+
+post('/words') do
+  Word.new(params.inject({}){|param,(k,v)| param[k.to_sym] = v; param}).save()
+  @words = Word.all()
+  erb(:success)
 end
--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -9,4 +9,19 @@ describe('the home path', {:type => :feature}) do
     visit('/')
     expect(page).to have_content("Welcome to Word Snob")
   end
+
+  it('should take you to the add word form') do
+    visit('/')
+    click_link("Add a Word")
+    expect(page).to have_content("Add a Word")
+  end
+end
+
+describe('the new word form path', {:type => :feature}) do
+  it('creates a new word from user input and renders success page') do
+    visit('/words/new')
+    fill_in("word", :with => "asymptote")
+    click_button("submit")
+    expect(page).to have_content("Success!")
+  end
 end
